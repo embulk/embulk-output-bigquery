4e0f5b0409a85e7c1e5a45ee213e6313acc99302
sakama <satoshiakama@gmail.com>
2015-04-02 17:57:56 +0900

    Modify IOException to FileNotFoundException when schema_path is not set.

--- src/main/java/org/embulk/output/BigqueryWriter.java
+++ src/main/java/org/embulk/output/BigqueryWriter.java
@@ -234,7 +234,7 @@ public class BigqueryWriter
     {
         if (autoCreateTable) {
             if (!schemaPath.isPresent()) {
-                throw new IOException("schema_path is empty");
+                throw new FileNotFoundException("schema_path is empty");
             } else {
                 File file = new File(schemaPath.orNull());
                 if (!file.exists()) {
