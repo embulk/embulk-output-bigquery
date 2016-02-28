86e28a802b05ed3afb6e453c2230e78f3c19e3b9
sonots <sonots@gmail.com>
2016-02-28 19:33:49 +0900

    remove old

--- lib/embulk/output/bigquery.rb
+++
@@ -1,3 +0,0 @@
-Embulk::JavaPlugin.register_output(
-  "bigquery", "org.embulk.output.BigqueryOutputPlugin",
-  File.expand_path('../../../../classpath', __FILE__))
--- src/test/java/org/embulk/output/TestBigqueryAuthentication.java
+++
@@ -1,5 +0,0 @@
-package org.embulk.output;
-
-public class TestBigqueryAuthentication
-{
-}
