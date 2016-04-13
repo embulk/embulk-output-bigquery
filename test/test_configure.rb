5dabd15b01f6996c57bbe2d3df0eca454db1cd9b
sonots <sonots@gmail.com>
2016-04-14 04:40:23 +0900

    fix tests

--- test/test_configure.rb
+++ test/test_configure.rb
@@ -61,7 +61,7 @@ module Embulk
         assert_equal 3600, task['job_status_max_polling_time']
         assert_equal 10, task['job_status_polling_interval']
         assert_equal false, task['is_skip_job_result_check']
-        assert_equal false, task['prevent_duplicate_insert']
+        assert_equal true, task['prevent_duplicate_insert']
         assert_equal false, task['with_rehearsal']
         assert_equal 1000, task['rehearsal_counts']
         assert_equal [], task['column_options']
