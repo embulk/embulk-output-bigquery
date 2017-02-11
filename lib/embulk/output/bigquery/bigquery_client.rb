73332f5bf2694bb2f51c6a223f859bbb23dc96eb
h_hara <hizu.login@gmail.com>
2017-02-11 20:33:03 +0900

    Also set schema_update_options in #load_from_gcs

--- lib/embulk/output/bigquery/bigquery_client.rb
+++ lib/embulk/output/bigquery/bigquery_client.rb
@@ -104,6 +104,11 @@ module Embulk
                   }
                 }
               }
+              
+              if @task['schema_update_options']
+                body[:configuration][:load][:schema_update_options] = @task['schema_update_options']
+              end
+              
               opts = {}
 
               Embulk.logger.debug { "embulk-output-bigquery: insert_job(#{@project}, #{body}, #{opts})" }
